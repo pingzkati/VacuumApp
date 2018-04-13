d9f67ebbddeae13f943fab87d948afd289c3f05c
Unknown <pingzkati@gmail.com>
2018-04-13 06:21:18 +0000

    eiei

--- app/src/main/java/com/warann/vacuum/vacuumapp/HistoryMainActivity.java
+++ app/src/main/java/com/warann/vacuum/vacuumapp/HistoryMainActivity.java
@@ -1,8 +1,40 @@
 package com.warann.vacuum.vacuumapp;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 
+public class HistoryMainActivity extends AppCompatActivity{
 
-public class HistoryMainActivity{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        int[] resId = {R.drawable.cell_shape, R.drawable.cell_shape,
+                R.drawable.cell_shape,R.drawable.cell_shape,
+                R.drawable.cell_shape};
+
+//        int[] resId = { R.drawable.aerithgainsborough
+//                , R.drawable.barretwallace, R.drawable.caitsith
+//                , R.drawable.cidhighwind, R.drawable.cloudstrife
+//                , R.drawable.redxiii, R.drawable.sephiroth
+//                , R.drawable.tifalockhart, R.drawable.vincentvalentine
+//                , R.drawable.yuffiekisaragi, R.drawable.zackfair };
+
+        String[] list = { "Date/Time", "Date/Time", "Date/Time"
+                , "Date/Time", "Date/Time"};
+
+        CustomAdapter adapter = new CustomAdapter(getApplicationContext(), list, resId);
+
+        ListView listView = (ListView)findViewById(R.id.listView1);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+
+            }
+        });
+    }
 
 }
--- app/src/main/java/com/warann/vacuum/vacuumapp/MainActivity.java
+++ app/src/main/java/com/warann/vacuum/vacuumapp/MainActivity.java
@@ -26,10 +26,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        Context mContext;
-        String[] strName;
-        int[] resId;
-
 
 //        Button start = (Button)findViewById(R.id.start);
 //
@@ -43,20 +39,34 @@ public class MainActivity extends AppCompatActivity {
 //        });
     }
 
-    public int getCount() {
-        return 0;
-    }
 
-    public Object getItem(int position) {
-        return null;
-    }
-
-    public long getItemId(int position) {
-        return 0;
-    }
-
-    public View getView(int position, View view, ViewGroup paernt) {
-        return null;
-    }
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.main);
+//
+//        int[] resId = {R.drawable.cell_shape, R.drawable.cell_shape,
+//                R.drawable.cell_shape,R.drawable.cell_shape,
+//                R.drawable.cell_shape};
+//
+////        int[] resId = { R.drawable.aerithgainsborough
+////                , R.drawable.barretwallace, R.drawable.caitsith
+////                , R.drawable.cidhighwind, R.drawable.cloudstrife
+////                , R.drawable.redxiii, R.drawable.sephiroth
+////                , R.drawable.tifalockhart, R.drawable.vincentvalentine
+////                , R.drawable.yuffiekisaragi, R.drawable.zackfair };
+//
+//        String[] list = { "Date/Time", "Date/Time", "Date/Time"
+//                , "Date/Time", "Date/Time"};
+//
+//        CustomAdapter adapter = new CustomAdapter(getApplicationContext(), list, resId);
+//
+//        ListView listView = (ListView)findViewById(R.id.listView1);
+//        listView.setAdapter(adapter);
+//        listView.setOnItemClickListener(new OnItemClickListener() {
+//            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+//
+//            }
+//        });
+//    }
 
 }
